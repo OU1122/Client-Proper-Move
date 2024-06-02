96556d1a04728fe2cedc4e37f788fd604e9c84ce
Randomnickname12 <eliquinoxltd@gmail.com>
2024-06-02 14:52:38 +0200

    Search Bar Added

--- src/components/searchBox.tsx
+++ src/components/searchBox.tsx
@@ -1,24 +1,55 @@
-import React, { FC } from "react";
+import React, { useState } from "react";
+import clsx from "clsx";
 
-const SearchBox: FC = () => {
+const SearchBox: React.FC = () => {
+	const [type, setType] = useState("Rent");
+
+	const changeType = (value: "Buy" | "Rent") => {
+		setType(value);
+	};
+
 	return (
-		<div>
+		<div className="mt-10">
 			<div>
-				<button>Buy</button>
-				<button>Rent</button>
+				<button
+					onClick={() => {
+						changeType("Buy");
+					}}
+					className={clsx("py-3 px-7 rounded-tl-md   border border-r-0", {
+						"bg-black text-white": type == "Buy",
+						"bg-white text-black": type == "Rent",
+					})}>
+					Buy
+				</button>
+				<button
+					onClick={() => {
+						changeType("Rent");
+					}}
+					className={clsx(
+						"py-3 px-7 rounded-tr-md  border-b-0 border border-l-0 ",
+						{
+							"bg-black text-white": type == "Rent",
+							"bg-white text-black": type == "Buy",
+						}
+					)}>
+					Rent
+				</button>
 			</div>
-			<form>
+			<form className="flex border rounded-bl-md">
 				<input
+					className="border-r-0  py-3 pl-7"
 					type="text"
 					name="location"
 					placeholder="City Location"></input>
 				<input
+					className="border-r-0  py-3"
 					type="number"
 					name="minPrice"
 					min={0}
 					max={10000000}
 					placeholder="Min Price"></input>
 				<input
+					className=" py-3"
 					type="number"
 					name="maxPrice"
 					min={0}
--- src/routes/homepage.tsx
+++ src/routes/homepage.tsx
@@ -1,9 +1,11 @@
+import SearchBox from "../components/searchBox";
+
 const HomePage: React.FC = () => {
 	return (
-		<div className="flex w-full h-screen mt-20 ">
-			<div className="text-div w-[60%] pr-[50px]">
+		<div className="flex flex-col-reverse sm:flex-row m-10  ">
+			<div className="text-div w-full sm:w-[60%] pr-[50px]">
 				<div>
-					<h1 className="text-4xl font-bold leading-snug pt-32">
+					<h1 className="text-4xl font-bold leading-snug pt-10 sm:pt-32">
 						Find Your Dream Place Faster Than You Can Say Zoopla
 					</h1>
 					<p className="pt-5">
@@ -13,7 +15,9 @@ const HomePage: React.FC = () => {
 						enim quae non!
 					</p>
 				</div>
-				<div>Searchbox</div>
+
+				<SearchBox />
+
 				<div className="flex justify-between mt-10 flex-wrap gap-5 sm:gap-0">
 					<div className="flex flex-col basis-1/3 ">
 						<span className="font-bold text-4xl tracking-wider">16+</span>
@@ -37,9 +41,9 @@ const HomePage: React.FC = () => {
 					</div>
 				</div>
 			</div>
-			<div className="img-div relative grow w-[40%] ">
+			<div className=" sm:relative h-auto w-full sm:w-[40%] flex justify-center ">
 				<img
-					className="w-full absolute top-0 right-0"
+					className="w-[300px] sm:w-full sm:absolute sm:top-0 sm:right-0"
 					src="/bg.png"></img>
 			</div>
 		</div>
