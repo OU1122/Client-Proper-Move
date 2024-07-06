1801bbd6582329fad72fe96ce4bb055595c3dec4
Randomnickname12 <eliquinoxltd@gmail.com>
2024-07-06 17:39:41 +0200

    Update apiRequest.tsx

--- src/lib/apiRequest.tsx
+++ src/lib/apiRequest.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-	baseURL: "http://localhost:8080/api",
+	baseURL: "https://propermove.azurewebsites.net/api",
 	withCredentials: true,
 });
 
